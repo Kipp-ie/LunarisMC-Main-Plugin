d334f4d22597a5e18d4508dbee41e2f940e6409f
Kipp-ie <Jenstheminer@gmail.com>
2023-08-25 21:52:03 +0200

    Initial commit

---
+++ src/main/java/dev/kippenboutske/nebulamc/main.java
@@ -0,0 +1,17 @@
+package dev.kippenboutske.nebulamc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
