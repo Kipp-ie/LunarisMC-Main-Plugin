3293f35db8af0dbad228d818fcba6fa5116ea904
Kipp-ie <Jenstheminer@gmail.com>
2023-08-29 00:50:57 +0200

    Added join and leave messages

--- src/main/java/dev/kippenboutske/nebulamc/listeners/joinListener.java
+++ src/main/java/dev/kippenboutske/nebulamc/listeners/joinListener.java
@@ -3,9 +3,11 @@ package dev.kippenboutske.nebulamc.listeners;
 import dev.kippenboutske.nebulamc.main;
 import jdk.tools.jmod.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.UUID;
 
@@ -17,8 +19,10 @@ public class joinListener implements Listener {
 
         main.createScoreBoard(e.getPlayer());
         e.getPlayer().sendTitle("[lang]title.welcome[/lang]", "BLABLABLA");
-
-
+        e.setJoinMessage(ChatColor.GRAY + "[ų] " + e.getPlayer().getDisplayName());
+    }
+    public void onLeave(PlayerQuitEvent e) {
+        e.setQuitMessage(ChatColor.GRAY + "[ť] " + e.getPlayer().getDisplayName());
     }
 
 }
