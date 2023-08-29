0c88e6f077a56950302bfcd113cc62afec6919a0
qBitComputer <qsuermondt@gmail.com>
2023-08-29 18:36:19 +0200

    idea for broadcast thingy

--- src/main/java/dev/kippenboutske/nebulamc/Broadcast.java
+++ src/main/java/dev/kippenboutske/nebulamc/Broadcast.java
@@ -16,9 +16,14 @@ public class Broadcast implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        StringBuilder Msg = new StringBuilder();
+        for (String a : args){
+            Msg.append(a).append(" ");
+        }
 
-        Bukkit.broadcastMessage(translateAlternateColorCodes('§', "§x§B§E§0§0§F§FN§x§A§3§0§0§F§Fe§x§8§9§0§0§F§Fb§x§6§E§0§0§F§Fu§x§5§3§0§0§F§Fl§x§3§8§0§0§F§Fa§x§1§E§0§0§F§FM§x§0§3§0§0§F§FC") + ChatColor.RESET + " " + translateAlternateColorCodes('§', Arrays.toString((args))));
+        Bukkit.broadcastMessage(translateAlternateColorCodes('§', "§x§B§E§0§0§F§FN§x§A§3§0§0§F§Fe§x§8§9§0§0§F§Fb§x§6§E§0§0§F§Fu§x§5§3§0§0§F§Fl§x§3§8§0§0§F§Fa§x§1§E§0§0§F§FM§x§0§3§0§0§F§FC") + ChatColor.RESET + " " + translateAlternateColorCodes('&', String Msg()));
         return true;
     }
+
 }
 
